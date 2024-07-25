52cf88e6501e64a2eff00339a9bb6005b0747241
matheus <matheusoliveira1991@hotmail.com>
2024-07-25 16:30:07 -0300

    spring

---
+++ src/main/java/br/com/api/consulta_anuncio/ConsultaAnuncioApplication.java
@@ -0,0 +1,13 @@
+package br.com.api.consulta_anuncio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsultaAnuncioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConsultaAnuncioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/api/consulta_anuncio/ConsultaAnuncioApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.api.consulta_anuncio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsultaAnuncioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
